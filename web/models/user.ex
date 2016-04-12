9c0f973ed75f2a576733d58a5fe6d9531e470aa4
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-12 22:33:22 +0800

    Chapger 4 completed.

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,14 @@ defmodule Rumbl.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+
+    timestamps
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
   end
 
 end
