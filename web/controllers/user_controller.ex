312462ef82a8c36efe0fd45853bf4b8b19cec2ff
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-13 22:50:08 +0800

    Stopped at :Presenting User Account Links"

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,14 +2,11 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(User)
+    render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
@@ -28,6 +25,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
 
@@ -36,14 +34,14 @@ defmodule Rumbl.UserController do
     end
   end
 
-  def authenticate(conn) do
+  def authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
       conn
       |> put_flash(:error, "You must be logged in to access that page")
       |> redirect(to: page_path(conn, :index))
-      |> halt
+      |> halt()
     end
   end
 
