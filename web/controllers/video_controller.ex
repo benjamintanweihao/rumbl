0c70d1043df141b3ecab4ad49c7c43449658b834
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-14 23:38:37 +0800

    Done with chapter 6

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -5,18 +5,25 @@ defmodule Rumbl.VideoController do
 
   plug :scrub_params, "video" when action in [:create, :update]
 
-  def index(conn, _params) do
-    videos = Repo.all(Video)
+  def index(conn, _params, user) do
+    videos = Repo.all(user_videos(user))
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset()
+
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset()
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -28,19 +35,19 @@ defmodule Rumbl.VideoController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+  def show(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id)
     render(conn, "show.html", video: video)
   end
 
   def edit(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video, video_params)
 
     case Repo.update(changeset) do
@@ -54,14 +61,21 @@ defmodule Rumbl.VideoController do
   end
 
   def delete(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
+    video = Repo.get!(user_videos(user), id)
     Repo.delete!(video)
 
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: video_path(conn, :index))
   end
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn),
+    [conn, conn.params, conn.assigns.current_user])
+  end
+
+  defp user_videos(user) do
+    assoc(user, :videos)
+  end
+
 end
