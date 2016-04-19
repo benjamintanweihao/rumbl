60cebaecb81b445695863b10bddb755e9285adba
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-04-20 07:29:07 +0800

    Stopped at "Testing Views and Templates"

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -12,4 +12,10 @@ defmodule Rumbl.TestHelpers do
     |> Rumbl.User.registration_changeset(changes)
     |> Repo.insert!()
   end
+
+  def insert_video(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:videos, attrs)
+    |> Repo.insert!()
+  end
 end
